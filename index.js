a3482ca64489175ce5e74ccdb7ed5a32b549f1c6
eustachekamala <eustachejared25@gmail.com>
2024-07-05 21:48:01 +0300

    I done the assignment that use the IIFE(Immetiatly-Invoqued Function Element that he use to auto-invoque a function, I created the function wrap also

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function saturdayFun( name ='roller-skate'){
+    return `This Saturday, I want to ${name}!`
+}
+
+function wrapAdjective(){
+    let result = '*';
+    let emphatic = 'a hard worker';
+        return (() => {
+            if(result === '*'){
+                return `You are ${result}${emphatic}${result}!`;
+            }
+            return (function wraps(){
+                result = '||';
+                return `You are ${result}${emphatic}${result}`; 
+            })
+        });
+}
+
+mondayWork();
+mondayWork('work at home');
+saturdayFun();
+wrapAdjective()()();
+
